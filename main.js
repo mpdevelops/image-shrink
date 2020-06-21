a1d0f904340117f8e9a5097e25b02dbea7a781c7
mpdevelops <mikepaiva@outlook.com>
2020-06-20 23:58:08 -0400

    launches window and renders html file

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require("electron");
+
+// set environment
+process.env.NODE_ENV = "development";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev,
+  });
+
+  mainWindow.loadFile("./app/index.html");
+}
+
+app.on("ready", createMainWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
