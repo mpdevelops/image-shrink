c2bf1b504402a81caa6fdb4956ef4c47d46c1e80
mpdevelops <mikepaiva@outlook.com>
2020-06-21 01:12:23 -0400

    added menu template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# image-shrink
+# Image Shrink
+
+Cross-platform desktop app built with Electron that takes JPG/PNG image as input and reduces file size based on user controlled size/quality scale.
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 // set environment
 process.env.NODE_ENV = "development";
@@ -20,7 +20,31 @@ function createMainWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", createMainWindow);
+app.on("ready", () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+});
+
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
+
+if (isMac) {
+  menu.unshift({ role: "appMenu" });
+}
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
